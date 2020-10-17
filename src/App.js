586e4cf69b57a635f87687b19c09452f9151d02f
Ori Levitan <orilevitan2@gmail.com>
2020-10-18 02:30:11 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>Some Title</header>
+      <div className="background1" />
+      <div className="background2" />
+      <div className="background3" />
+      <div className="background4" />
+      <footer>lorum ipsum</footer>
+    </div>
+  );
+}
+
+export default App;
