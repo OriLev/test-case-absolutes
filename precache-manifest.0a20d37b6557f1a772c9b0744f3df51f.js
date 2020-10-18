b3dc7fefd6b66f2d10969971fa88b1c298825078
Ori Levitan <orilevitan2@gmail.com>
2020-10-18 03:47:49 +0300

    Updates

---
+++ precache-manifest.0a20d37b6557f1a772c9b0744f3df51f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a80a546ee03c6c3f7046c628ac578e96",
+    "url": "/test-case-absolutes/index.html"
+  },
+  {
+    "revision": "5cf306444175a2dfa37f",
+    "url": "/test-case-absolutes/static/css/main.fda9b12e.chunk.css"
+  },
+  {
+    "revision": "89e7e611a9f9643a33e2",
+    "url": "/test-case-absolutes/static/js/2.63975590.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/test-case-absolutes/static/js/2.63975590.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5cf306444175a2dfa37f",
+    "url": "/test-case-absolutes/static/js/main.75618438.chunk.js"
+  },
+  {
+    "revision": "62d72d88b71d2153d1e3",
+    "url": "/test-case-absolutes/static/js/runtime-main.e7f67e9f.js"
+  }
+]);
