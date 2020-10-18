091b46470493df28bc6345018cf592e1801ebf2f
Ori Levitan <orilevitan2@gmail.com>
2020-10-18 03:15:50 +0300

    add animated backgrounds

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header>Some Title</header>
+      <header className="App-header">Some Title</header>
       <div className="background1" />
       <div className="background2" />
       <div className="background3" />
       <div className="background4" />
-      <footer>lorum ipsum</footer>
+      <footer className="App-footer">lorum ipsum</footer>
     </div>
   );
 }
